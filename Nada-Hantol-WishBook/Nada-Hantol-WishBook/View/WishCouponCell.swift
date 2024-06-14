79e15c9728cb6e1de928bdc244e51c6ca0df0ac4
yajikim <yejikim1022@gmail.com>
2024-06-15 03:46:33 +0900

    [Feat] 소원권 기본 UI 구성

--- Nada-Hantol-WishBook/Nada-Hantol-WishBook/View/WishCouponCell.swift
+++ Nada-Hantol-WishBook/Nada-Hantol-WishBook/View/WishCouponCell.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct WishCouponCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            VStack(alignment: .leading){
+                Text("도서관에서 얼음 떠다주기")
+                    .systemFont(.semiBold, 18)
+                    .padding(.bottom, 4)
+                Text("₩150,000")
+                    .systemFont(.bold, 17)
+                    .padding(.bottom, 8)
+                    .foregroundColor(.point)
+                Text("Nada")
+                    .systemFont(.medium, 14)
+                    .padding(.bottom, 15)
+                Button {
+                    
+                } label: {
+                    Text("구매하기")
+                        .systemFont(.bold, 14)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(Color(.pointSecondary))
+                        .cornerRadius(8)
+                }
+                
+                .foregroundColor(.point)
+                
+                
+            }
+            Text("🧊")
+                .systemFont(.bold, 77)
+                .offset(y: 22)
+        }
+        .padding(24)
+        .background(Color(.fieldBG))
+        .cornerRadius(12)
+        
     }
 }
 
