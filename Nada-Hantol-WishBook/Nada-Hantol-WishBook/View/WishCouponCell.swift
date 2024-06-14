7f1cacb0eb4a6032f211ddb1f7b83a4609e83b0e
yajikim <yejikim1022@gmail.com>
2024-06-14 12:32:45 +0900

    Test

---
+++ Nada-Hantol-WishBook/Nada-Hantol-WishBook/View/WishCouponCell.swift
@@ -0,0 +1,18 @@
+//
+//  WishCouponCell.swift
+//  Nada-Hantol-WishBook
+//
+//  Created by YejiKim on 6/14/24.
+//
+
+import SwiftUI
+
+struct WishCouponCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WishCouponCell()
+}
