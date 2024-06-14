e067d0c43c904c996ff782e2d91c03a089453c3b
한톨 <113565086+thinkySide@users.noreply.github.com>
2024-06-14 10:40:43 +0900

    Xcode 기본 파일 생성

---
+++ Nada-Hantol-WishBook/Nada-Hantol-WishBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nada-Hantol-WishBook
+//
+//  Created by 김민준 on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nada-Hantol-WishBook/Nada-Hantol-WishBook/Nada_Hantol_WishBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nada_Hantol_WishBookApp.swift
+//  Nada-Hantol-WishBook
+//
+//  Created by 김민준 on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Nada_Hantol_WishBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
