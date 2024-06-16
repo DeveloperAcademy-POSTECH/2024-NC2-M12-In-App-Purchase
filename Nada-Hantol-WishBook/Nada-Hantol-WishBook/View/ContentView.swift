0863cb82b456f39358458b9dabf1d021b5373ff4
thinkyside <eunlyuing@gmail.com>
2024-06-16 15:36:33 +0900

    [FEAT] CouponInfoSheetView 컴포넌트화

--- Nada-Hantol-WishBook/Nada-Hantol-WishBook/View/ContentView.swift
+++ Nada-Hantol-WishBook/Nada-Hantol-WishBook/View/ContentView.swift
@@ -8,19 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var infoSheetPresented = false
+    
     var body: some View {
-        Text("나다와 한톨")
-            .systemFont(.bold, 20)
-            .foregroundStyle(.point)
-        
-        ActionButton(
-            title: "구매한 쿠폰 확인하기",
-            buttonType: .purchaseCoupon,
-            tapAction: {
-                // TODO: 쿠폰 구매 화면 present
-            }
-        )
-        .padding(.horizontal, 24)
+        VStack {
+            Text("나다와 한톨")
+                .systemFont(.bold, 20)
+                .foregroundStyle(.point)
+            
+            IconButton(
+                buttonType: .info,
+                tapAction: {
+                    infoSheetPresented.toggle()
+                }
+            )
+            
+            ActionButton(
+                title: "구매한 쿠폰 확인하기",
+                buttonType: .purchaseCoupon,
+                tapAction: {
+                    // TODO: 쿠폰 구매 화면 present
+                }
+            )
+            .padding(.horizontal, 24)
+        }
+        .sheet(isPresented: $infoSheetPresented) {
+            CouponInfoSheetView()
+                .presentationDetents([.height(240)])
+                .presentationDragIndicator(.visible)
+        }
     }
 }
 
